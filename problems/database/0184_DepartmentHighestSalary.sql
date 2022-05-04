7306cee84231df026ea7089fc3c4a0ac54003429
Ung Tsu Han <tsuhan.ung@emerson.com>
2022-05-04 20:26:08 +0800

    refactoring

--- problems/database/0184_DepartmentHighestSalary.sql
+++ problems/database/0184_DepartmentHighestSalary.sql
@@ -1,12 +1,12 @@
 WITH max_salary_by_dept AS
 (
-    SELECT departmentId, MAX(salary) as salary FROM Employee
+    SELECT departmentId, MAX(salary) AS salary FROM Employee
     GROUP BY departmentId
 )
 SELECT 
-    d.name as Department,
-    e.name as Employee,
-    e.salary as Salary
+    d.name AS Department,
+    e.name AS Employee,
+    e.salary AS Salary
 FROM Employee e
 JOIN max_salary_by_dept m ON m.salary = e.salary AND m.departmentId = e.departmentId
 LEFT JOIN Department d ON e.departmentId = d.id
